2ddd9795291f6252ab98fe31556c2b875eaddcdf
nf-projects <90344282+nf-projects@users.noreply.github.com>
2025-01-27 10:39:52 -0800

    mess it up

--- app/nils/page.tsx
+++ app/nils/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					<Image
+					<Imag
 						aria-hidden
 						src="https://nextjs.org/icons/window.svg"
 						alt="Window icon"
