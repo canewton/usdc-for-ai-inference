bd8ad19f707fe2c802001754398e92aa5e700f5d
varun22 <84626068+varun22@users.noreply.github.com>
2025-02-03 13:21:51 -0800

    removed tutorial components, unstable build
    fails auth since there are no sample env variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
                       <DeployButton />
                     </div>
                   </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                  <HeaderAuth />
                 </div>
               </nav>
               <div className="flex flex-col gap-20 max-w-5xl p-5">
