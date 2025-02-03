d380a1fcaedbaf60e271b9a0d30e1ef40e837871
Borna <niknahad.borna@gmail.com>
2025-02-02 19:31:36 -0800

    update env name

--- app/api/generateimagedalle/route.ts
+++ app/api/generateimagedalle/route.ts
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
 const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY! });
