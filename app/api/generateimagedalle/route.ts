4306733571ba0f72edcb0c457e47d1b7dc6eae8d
Borna <niknahad.borna@gmail.com>
2025-02-03 18:43:56 -0800

    change create client to utils

--- app/api/generateimagedalle/route.ts
+++ app/api/generateimagedalle/route.ts
@@ -1,11 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
 import { NextResponse } from 'next/server';
 import { v4 as uuidv4 } from 'uuid';
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-);
+import { createClient } from '@/utils/supabase/client';
+
+const supabase = createClient();
 
 export async function POST(req: Request) {
   try {
