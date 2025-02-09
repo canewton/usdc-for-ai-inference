1f36cd90f3854fac7b7bf01a3af941086b85b8c4
Caden Newton <cnewton@ucdavis.edu>
2025-02-09 00:26:56 -0800

    Add developer controlled wallet with dashboard ui

---
+++ components/ui/copy-button.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { Copy } from 'lucide-react';
+import { type FunctionComponent, useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { sleep } from '@/utils/sleep';
+
+interface Props {
+  text: string;
+}
+
+export const CopyButton: FunctionComponent<Props> = (props) => {
+  const [shouldShowTooltip, setShouldShowTooltip] = useState(false);
+
+  const simulateTooltipOpening = async () => {
+    setShouldShowTooltip(true);
+    await sleep(700);
+    setShouldShowTooltip(false);
+  };
+
+  return (
+    <TooltipProvider>
+      <Tooltip open={shouldShowTooltip}>
+        <TooltipTrigger asChild>
+          <Button onClick={simulateTooltipOpening}>
+            <Copy
+              className="h-4 w-4"
+              onClick={() => navigator.clipboard.writeText(props.text)}
+            />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Copied</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
