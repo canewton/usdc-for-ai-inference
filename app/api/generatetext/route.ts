ce2d271c6640b6a2e4dc7354b388ba1c5576538e
k8tlynvo <kqvo@ucdavis.edu>
2025-03-02 11:30:14 -0800

    Generate text endpoint edits

--- app/api/generatetext/route.ts
+++ app/api/generatetext/route.ts
@@ -1,21 +1,27 @@
 import { openai } from '@ai-sdk/openai';
-import { streamText} from "ai"
+import { streamText } from 'ai';
+import { NextResponse } from 'next/server';
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
+  try {
+    // Parse req body
+    const { messages, model } = await req.json();
 
-  // Parse req body
-  const { messages } = await req.json();
+    // Get result
+    const result = streamText({
+      model: openai(model),
+      messages,
+    });
 
-  // Get result
-  const result = streamText({
-    model: openai('gpt-4o-mini'),
-    messages,
-  }); 
-
-  // Return result
-  return result.toDataStreamResponse();
-  
+    // Return result
+    return result.toDataStreamResponse();
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Failed to generate text' },
+      { status: 500 },
+    );
+  }
 }
