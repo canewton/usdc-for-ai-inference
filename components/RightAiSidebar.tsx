822c820b52e0c5b3960523ac7135875cbe2ac568
k8tlynvo <kqvo@ucdavis.edu>
2025-03-16 15:11:46 -0700

    Standard main and right section components for ai pages

---
+++ components/RightAiSidebar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function RightAiSidebar({
+  children,
+  isImageInput
+}: {
+  children: React.ReactNode;
+  isImageInput: boolean,
+}) {
+  return (
+    <aside className={`flex ${isImageInput ? 'w-80' : 'w-60'} flex-col items-center p-6 border border-gray-200 rounded-l-3xl h-full bg-section`}>
+      {children}
+    </aside>
+  )
+}
