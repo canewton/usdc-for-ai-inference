75875766fd3eefdfaab8818408fe6859b0a45ca4
Leo Ying <leoleoying@gmail.com>
2025-03-31 00:04:57 -0700

    Edit leftsidebar and download

--- components/3d/canvas.tsx
+++ components/3d/canvas.tsx
@@ -5,7 +5,6 @@ import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react';
 
 import { Button } from '@/components/ui/button';
-import DownloadIcon from '@/public/download.svg';
 
 interface CanvasAreaProps {
   modelUrl: string | null;
@@ -77,11 +76,21 @@ export default function CanvasArea({
               disabled={isLoading}
               aria-label="Download 3D Model"
             >
-              <img
-                src={DownloadIcon}
-                alt="Download Icon"
-                className="w-6 h-6 text-white"
-              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                <polyline points="7 10 12 15 17 10"></polyline>
+                <line x1="12" y1="15" x2="12" y2="3"></line>
+              </svg>
             </button>
           </Suspense>
           <div className="mt-2">
