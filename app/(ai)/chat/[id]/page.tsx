a00f90b48b1d4f61851cb07970eabc5b503171d2
Caden Newton <cnewton@ucdavis.edu>
2025-04-28 19:26:52 -0700

    minor fix for build issue

--- app/(ai)/chat/[id]/page.tsx
+++ app/(ai)/chat/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useParams } from 'next/navigation';
 
-import { Chat } from '@/components/chat';
+import { Chat } from '@/components/Chat';
 
 export default function ChatPage() {
   const params = useParams();
