038164f2e200284e077bd7f68ad808257f3929c4
Caden Newton <cnewton@ucdavis.edu>
2025-05-02 06:42:26 -0700

    aesthetic hotfixes

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -22,7 +22,7 @@ export default async function Signup(props: {
   return (
     <>
       <form className="flex flex-col min-w-64 max-w-64 mx-auto">
-        <h1 className="text-2xl font-medium">Sign up</h1>
+        <h1 className="text-2xl font-semibold">Sign up</h1>
         <p className="text-sm text text-foreground">
           Already have an account?{' '}
           <Link className="text-primary font-medium underline" href="/sign-in">
