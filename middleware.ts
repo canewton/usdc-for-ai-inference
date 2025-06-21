b6882a50c388c28521ecfbc870e184b67b2ac7d0
Caden Newton <cnewton@ucdavis.edu>
2025-06-20 21:38:53 -0700

    simplify middleware for testing

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 // middleware.ts
 import { createServerClient } from '@supabase/ssr';
-import { cookies } from 'next/headers';
 import { type NextRequest, NextResponse } from 'next/server';
 
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
@@ -13,23 +12,7 @@ const corsOptions = {
 };
 
 export async function middleware(request: NextRequest) {
-  console.log('Middleware triggered for:', request.nextUrl.pathname);
-  // Check the origin from the request
-  const origin = request.headers.get('origin') ?? '';
-  const isAllowedOrigin = origin === baseUrl;
-
-  // Handle preflighted requests
-  const isPreflight = request.method === 'OPTIONS';
-
-  if (isPreflight) {
-    const preflightHeaders = {
-      ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
-      ...corsOptions,
-    };
-    return NextResponse.json({}, { headers: preflightHeaders });
-  }
-
-  let response = NextResponse.next({
+  let supabaseResponse = NextResponse.next({
     request,
   });
   const supabase = createServerClient(
@@ -44,11 +27,11 @@ export async function middleware(request: NextRequest) {
           cookiesToSet.forEach(({ name, value, options }) =>
             request.cookies.set(name, value),
           );
-          response = NextResponse.next({
+          supabaseResponse = NextResponse.next({
             request,
           });
           cookiesToSet.forEach(({ name, value, options }) =>
-            response.cookies.set(name, value, options),
+            supabaseResponse.cookies.set(name, value, options),
           );
         },
       },
@@ -61,75 +44,29 @@ export async function middleware(request: NextRequest) {
   const {
     data: { user },
   } = await supabase.auth.getUser();
-
-  if (isAllowedOrigin) {
-    response.headers.set('Access-Control-Allow-Origin', origin);
-  }
-
-  Object.entries(corsOptions).forEach(([key, value]) => {
-    response.headers.set(key, value);
-  });
-
-  const cookie = (await cookies()).getAll();
-  console.log('Cookies:', cookie);
-
-  const { pathname } = request.nextUrl;
-
-  // Define protected routes
-  const protectedRoutes = [
-    '/dashboard',
-    '/admin',
-    '/chat',
-    '/3d',
-    '/video',
-    '/image',
-  ];
-  const adminRoutes = ['/admin'];
-  const userOnlyRoutes = ['/dashboard', '/3d', '/chat', '/image', '/video'];
-  const authRoutes = ['/sign-in', '/sign-up', '/forgot-password'];
-
-  const isProtectedRoute = protectedRoutes.some((route) =>
-    pathname.startsWith(route),
-  );
-  const isAdminRoute = adminRoutes.some((route) => pathname.startsWith(route));
-  const isUserOnlyRoute = userOnlyRoutes.some(
-    (route) => pathname === route || pathname.startsWith(route),
-  );
-  const isAuthRoute = authRoutes.some((route) => pathname.startsWith(route));
-
-  console.log('User:', user);
-  if (!user && (isProtectedRoute || request.nextUrl.pathname === '/')) {
-    // Redirect unauthenticated users trying to access protected routes
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith('/sign-in') &&
+    !request.nextUrl.pathname.startsWith('/sign-up')
+  ) {
+    // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = '/sign-in';
     return NextResponse.redirect(url);
   }
-  console.log('not going to sign-in');
-
-  if (user) {
-    // Fetch profile info to check admin status
-    const { data: profile } = await supabase
-      .from('profiles')
-      .select('is_admin')
-      .eq('auth_user_id', user.id)
-      .single();
-
-    // Admin user specific redirects
-    if (profile?.is_admin) {
-      if (request.nextUrl.pathname === '/' || isUserOnlyRoute || isAuthRoute) {
-        return NextResponse.redirect(new URL('/admin', request.url));
-      }
-    } else {
-      if (request.nextUrl.pathname === '/' || isAdminRoute || isAuthRoute) {
-        const url = request.nextUrl.clone();
-        url.pathname = '/dashboard';
-        return NextResponse.redirect(url);
-      }
-    }
-  }
-
-  console.log('response', response);
-  return response;
+  // IMPORTANT: You *must* return the supabaseResponse object as it is.
+  // If you're creating a new response object with NextResponse.next() make sure to:
+  // 1. Pass the request in it, like so:
+  //    const myNewResponse = NextResponse.next({ request })
+  // 2. Copy over the cookies, like so:
+  //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
+  // 3. Change the myNewResponse object to fit your needs, but avoid changing
+  //    the cookies!
+  // 4. Finally:
+  //    return myNewResponse
+  // If this is not done, you may be causing the browser and server to go out
+  // of sync and terminate the user's session prematurely!
+  return supabaseResponse;
 }
 
 export const config = {
