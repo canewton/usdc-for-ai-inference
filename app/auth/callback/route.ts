991719fc67f2840bcd67bd73e4054fad386b78fc
Caden Newton <cnewton@ucdavis.edu>
2025-06-27 19:31:13 -0700

    testing

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,93 +1,29 @@
-import { NextResponse } from 'next/server';
+import { type EmailOtpType } from '@supabase/supabase-js';
+import { redirect } from 'next/navigation';
+import { type NextRequest } from 'next/server';
 
 import { createClient } from '@/utils/supabase/server';
 
-const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : 'http://localhost:3000';
-
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
+  console.log('auth/callback/route.ts: GET called');
   const { searchParams } = new URL(request.url);
+  const token_hash = searchParams.get('token_hash');
+  const type = searchParams.get('type') as EmailOtpType | null;
+  const next = searchParams.get('next') ?? '/';
 
-  const code = searchParams.get('code');
-
-  const nextUrl = searchParams.get('next') ?? '/';
-
-  if (code) {
+  if (token_hash && type) {
     const supabase = await createClient();
 
-    const { data, error } = await supabase.auth.exchangeCodeForSession(code);
-
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    });
     if (!error) {
-      const { data: user, error: userIdError } = await supabase
-        .from('profiles')
-        .update({ email: data.user.email })
-        .eq('auth_user_id', data.user.id)
-        .select('id')
-        .single();
-
-      if (userIdError) {
-        console.error(
-          'Could not find an user with such auth_user_id',
-          userIdError,
-        );
-        return NextResponse.json(
-          { message: 'Could not find an user with such auth_user_id' },
-          { status: 500 },
-        );
-      }
-
-      const { data: walletAlreadyExists } = await supabase
-        .from('wallets')
-        .select()
-        .eq('profile_id', user.id)
-        .single();
-
-      if (walletAlreadyExists) {
-        return NextResponse.redirect(`${baseUrl}/${nextUrl}`);
-      }
-
-      const createdWalletSetResponse = await fetch(
-        `${baseUrl}/api/wallet-set`,
-        {
-          method: 'PUT',
-          body: JSON.stringify({
-            entityName: data.user.email,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        },
-      );
-
-      const createdWalletSet = await createdWalletSetResponse.json();
-
-      const createdWalletResponse = await fetch(`${baseUrl}/api/wallet`, {
-        method: 'POST',
-        body: JSON.stringify({
-          walletSetId: createdWalletSet.id,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      const createdWallet = await createdWalletResponse.json();
-
-      await supabase.schema('public').from('wallets').upsert({
-        profile_id: user.id,
-        circle_wallet_id: createdWallet.id,
-        wallet_type: createdWallet.custodyType,
-        wallet_set_id: createdWalletSet.id,
-        wallet_address: createdWallet.address,
-        account_type: createdWallet.accountType,
-        blockchain: createdWallet.blockchain,
-        currency: 'USDC',
-      });
-
-      return NextResponse.redirect(`${baseUrl}/${nextUrl}`);
+      // redirect user to specified redirect URL or root of app
+      redirect(next);
     }
   }
 
-  return NextResponse.redirect(`${baseUrl}/auth/auth-error`);
+  // redirect the user to an error page with some instructions
+  redirect('/error');
 }
